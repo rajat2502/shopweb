e787a0c44053e49e42063f5d210e4a3afef290cf
abc <rajat2502@users.noreply.github.com>
2019-03-18 00:12:57 +0530

    few essential edits

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-var a=getElementsByClassName('far fa-heart');
+window.onscroll=function(){
+  scroll();
+}
+
+function scroll(){
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20)
+  {
+    document.getElementById("mybtn").style.display="block";
+  }
+  else{
+    document.getElemtById("mybtn").style.display="none";
+  }
+}
+
+function backtotop()
+{
+  document.body.scrollTop =0;
+  document.documentElement.scrollTop=0;
+}
